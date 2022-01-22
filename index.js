c2a784a2d156654ae1ee99d6cb27bbb7ebdde2e9
Santosh <santoshghalan@gmail.com>
2022-01-22 14:32:56 +1100

    commit tree 56d1f651d0d742c02623cb46267c326ee50b6e76

---
+++ index.js
@@ -0,0 +1,23 @@
+function add(a,b) {
+    return (a + b);
+}
+
+function subtract(a,b) {
+    return (a - b);
+}
+
+function multiply(a,b) {
+    return (a * b);
+}
+
+function divide(a,b){
+    return (a / b);
+}
+
+funtion increment(n) {
+   return (n++);
+}
+
+increment();
+
+
